fdf0d68b785c006d2345a3b7e921967b7929dee3
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2018-12-31 23:21:39 +0900

    Fixed dropout

--- qiqc/models/encoder/rnn.py
+++ qiqc/models/encoder/rnn.py
@@ -6,23 +6,27 @@ class LSTMEncoder(nn.Module):
     def __init__(self, config):
         super().__init__()
         rnns = []
+        dropouts = []
         input_size = config['n_input']
         for i in range(config['n_layers']):
             rnn = nn.LSTM(
                 input_size=input_size,
                 hidden_size=config['n_hidden'],
-                dropout=config['dropout'],
                 bidirectional=config['bidirectional'],
                 batch_first=True,
             )
+            dropout = nn.Dropout(config['dropout'])
             input_size = config['out_scale'] * config['n_hidden']
             rnns.append(rnn)
+            dropouts.append(dropout)
         self.rnns = nn.ModuleList(rnns)
+        self.dropouts = nn.ModuleList(dropouts)
 
     def forward(self, input):
         h = input
-        for rnn in self.rnns:
+        for rnn, dropout in zip(self.rnns, self.dropouts):
             h, _ = rnn(h)
+            h = dropout(h)
         h = h.contiguous()
         return h
 
@@ -32,6 +36,7 @@ class LSTMGRUEncoder(nn.Module):
     def __init__(self, config):
         super().__init__()
         rnns = []
+        dropouts = []
         input_size = config['n_input']
         assert config['n_layers'] >= 2
         for i in range(config['n_layers'] - 1):
@@ -42,8 +47,10 @@ class LSTMGRUEncoder(nn.Module):
                 bidirectional=config['bidirectional'],
                 batch_first=True,
             )
+            dropout = nn.Dropout(config['dropout'])
             input_size = config['out_scale'] * config['n_hidden']
             rnns.append(rnn)
+            dropouts.append(dropout)
         rnn = nn.GRU(
             input_size=input_size,
             hidden_size=config['n_hidden'],
@@ -53,6 +60,7 @@ class LSTMGRUEncoder(nn.Module):
         )
         rnns.append(rnn)
         self.rnns = nn.ModuleList(rnns)
+        self.dropouts = nn.ModuleList(dropouts)
 
     def forward(self, input):
         h = input
