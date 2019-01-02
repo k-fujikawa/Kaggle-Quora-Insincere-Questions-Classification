601ce47400cdc3b9749b60c4d3acb94bf17bebfb
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2019-01-03 00:32:37 +0900

    Support to skip mask

--- qiqc/models/aggregator/pooling.py
+++ qiqc/models/aggregator/pooling.py
@@ -6,9 +6,10 @@ from torch import nn
 class MaxPoolingAggregator(nn.Module):
 
     def __call__(self, hs, mask):
-        _mask = mask[:, :, None].repeat(1, 1, hs.shape[-1])
-        fill_value = torch.full(hs.shape, -np.inf).to(hs.device)
-        hs = torch.where(_mask, hs, fill_value)
+        if mask is not None:
+            _mask = mask[:, :, None].repeat(1, 1, hs.shape[-1])
+            fill_value = torch.full(hs.shape, -np.inf).to(hs.device)
+            hs = torch.where(_mask, hs, fill_value)
         h = hs.max(dim=1)[0]
         return h
 
@@ -16,9 +17,10 @@ class MaxPoolingAggregator(nn.Module):
 class SumPoolingAggregator(nn.Module):
 
     def __call__(self, hs, mask):
-        _mask = mask[:, :, None].repeat(1, 1, hs.shape[-1])
-        fill_value = torch.zeros(hs.shape).to(hs.device)
-        hs = torch.where(_mask, hs, fill_value)
+        if mask is not None:
+            _mask = mask[:, :, None].repeat(1, 1, hs.shape[-1])
+            fill_value = torch.zeros(hs.shape).to(hs.device)
+            hs = torch.where(_mask, hs, fill_value)
         h = hs.sum(dim=1)
         return h
 
@@ -26,9 +28,10 @@ class SumPoolingAggregator(nn.Module):
 class AvgPoolingAggregator(nn.Module):
 
     def __call__(self, hs, mask):
-        _mask = mask[:, :, None].repeat(1, 1, hs.shape[-1])
-        fill_value = torch.zeros(hs.shape).to(hs.device)
-        hs = torch.where(_mask, hs, fill_value)
+        if mask is not None:
+            _mask = mask[:, :, None].repeat(1, 1, hs.shape[-1])
+            fill_value = torch.zeros(hs.shape).to(hs.device)
+            hs = torch.where(_mask, hs, fill_value)
         h = hs.sum(dim=1)
         maxlen = mask.sum(dim=1)
         h /= maxlen[:, None].type(torch.float)
