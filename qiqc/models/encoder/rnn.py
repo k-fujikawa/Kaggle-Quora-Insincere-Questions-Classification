91e809ebeba96fb492e7498e267118dde7c14d63
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2019-01-03 00:31:25 +0900

    Support mask / pack options for RNNEncoder

--- qiqc/models/encoder/rnn.py
+++ qiqc/models/encoder/rnn.py
@@ -1,70 +1,56 @@
+import torch
 from torch import nn
 
 
-class LSTMEncoder(nn.Module):
+class RNNEncoderBase(nn.Module):
 
-    def __init__(self, config):
+    def __init__(self, config, modules, pack=False, mask=False):
         super().__init__()
+        self.pack = pack
+        self.mask = mask
         rnns = []
-        dropouts = []
         input_size = config['n_input']
-        for i in range(config['n_layers']):
-            rnn = nn.LSTM(
+        for module in modules:
+            rnn = module(
                 input_size=input_size,
                 hidden_size=config['n_hidden'],
                 bidirectional=config['bidirectional'],
                 batch_first=True,
             )
-            dropout = nn.Dropout(config['dropout'])
             input_size = config['out_scale'] * config['n_hidden']
             rnns.append(rnn)
-            dropouts.append(dropout)
         self.rnns = nn.ModuleList(rnns)
-        self.dropouts = nn.ModuleList(dropouts)
 
-    def forward(self, input):
+    def forward(self, input, mask):
         h = input
-        for rnn, dropout in zip(self.rnns, self.dropouts):
-            h, _ = rnn(h)
-            h = dropout(h)
-        h = h.contiguous()
-        return h
-
-
-class LSTMGRUEncoder(nn.Module):
-
-    def __init__(self, config):
-        super().__init__()
-        rnns = []
-        dropouts = []
-        input_size = config['n_input']
-        assert config['n_layers'] >= 2
-        for i in range(config['n_layers'] - 1):
-            rnn = nn.LSTM(
-                input_size=input_size,
-                hidden_size=config['n_hidden'],
-                dropout=config['dropout'],
-                bidirectional=config['bidirectional'],
-                batch_first=True,
-            )
-            dropout = nn.Dropout(config['dropout'])
-            input_size = config['out_scale'] * config['n_hidden']
-            rnns.append(rnn)
-            dropouts.append(dropout)
-        rnn = nn.GRU(
-            input_size=input_size,
-            hidden_size=config['n_hidden'],
-            dropout=config['dropout'],
-            bidirectional=config['bidirectional'],
-            batch_first=True,
-        )
-        rnns.append(rnn)
-        self.rnns = nn.ModuleList(rnns)
-        self.dropouts = nn.ModuleList(dropouts)
-
-    def forward(self, input):
-        h = input
+        if self.pack:
+            seq_lengths, perm_idx = mask.sum(dim=1).sort(0, descending=True)
+            h = torch.nn.utils.rnn.pack_padded_sequence(
+                h[perm_idx], seq_lengths, batch_first=True)
         for rnn in self.rnns:
+            if self.mask:
+                _mask = mask[:, :, None].repeat(1, 1, h.shape[-1])
+                fill_value = torch.full(h.shape, 0).to(h.device)
+                h = torch.where(_mask, h, fill_value)
             h, _ = rnn(h)
-        h = h.contiguous()
+
+        if self.pack:
+            h, _ = torch.nn.utils.rnn.pad_packed_sequence(
+                h, batch_first=True)
+            h = h[perm_idx.argsort()]
         return h
+
+
+class LSTMEncoder(RNNEncoderBase):
+
+    def __init__(self, config):
+        modules = [nn.LSTM] * config['n_layers']
+        super().__init__(config, modules)
+
+
+class LSTMGRUEncoder(RNNEncoderBase):
+
+    def __init__(self, config):
+        assert config['n_layers'] > 1
+        modules = [nn.LSTM] * (config['n_layers'] - 1) + [nn.GRU]
+        super().__init__(config, modules)
