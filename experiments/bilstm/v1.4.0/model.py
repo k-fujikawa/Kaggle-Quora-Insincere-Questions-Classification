7c0592a08841418f5fed2cde9ee7d24d89b3d590
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2019-01-09 21:52:06 +0900

    Update fine-tuning logic

--- experiments/bilstm/v1.4.0/model.py
+++ experiments/bilstm/v1.4.0/model.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from gensim.models import Word2Vec
 from torch import nn
 
 from qiqc.builder import build_aggregator
@@ -9,23 +10,42 @@ from qiqc.models import EmbeddingUnit
 from qiqc.models import BinaryClassifier
 
 
-def build_models(config, word_freq, token2id, pretrained_vectors):
+def build_models(config, word_freq, token2id, pretrained_vectors, all_df):
     models = []
+    pos_weight = torch.FloatTensor([config['pos_weight']]).to(config['device'])
+    if config['embedding']['finetune']:
+        initial_vectors, unk_freq = build_embedding(
+            config, word_freq, pretrained_vectors)
+        finetuned_vectors = finetune_embedding(
+            config, word_freq, token2id, initial_vectors, all_df)
+        unk_ids = np.array([
+            token2id[token] for token, freq in unk_freq.items()
+            if freq < config['vocab']['min_count']])
+        finetuned_vectors[0] = 0
+        if len(unk_ids) > 0:
+            finetuned_vectors[unk_ids] = 0
+
     for i in range(config['cv']):
-        embedding, unk_freq = build_embedding(
-            config, word_freq, token2id, pretrained_vectors)
-        model = build_model(config, embedding)
+        initial_vectors, unk_freq = build_embedding(
+            config, word_freq, pretrained_vectors)
+        if config['embedding']['finetune']:
+            initial_vectors = (initial_vectors + finetuned_vectors) / 2
+        embedding = nn.Embedding.from_pretrained(
+            torch.Tensor(initial_vectors), freeze=True)
+        lossfunc = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+        model = build_model(config, embedding, lossfunc)
         models.append(model)
+
     return models, unk_freq
 
 
-def build_model(config, embedding):
+def build_model(config, embedding, lossfunc):
     encoder = Encoder(config['model'], embedding)
-    clf = BinaryClassifier(config['model'], encoder)
+    clf = BinaryClassifier(config['model'], encoder, lossfunc)
     return clf
 
 
-def build_embedding(config, word_freq, token2id, pretrained_vectors):
+def build_embedding(config, word_freq, pretrained_vectors):
     initial_vectors, unk_freqs = [], []
     for name, _pretrained_vectors in pretrained_vectors.items():
         vec, known_freq, unk_freq = build_word_vectors(
@@ -33,23 +53,19 @@ def build_embedding(config, word_freq, token2id, pretrained_vectors):
         initial_vectors.append(vec)
         unk_freqs.append(unk_freq)
     initial_vectors = np.array(initial_vectors).mean(axis=0)
+    return initial_vectors, unk_freq
 
-    if config['embedding']['finetune']:
-        unfixed_tokens = set([token for token, freq in unk_freq.items()
-                             if freq >= config['vocab']['min_count']])
-        fixed_idxmap = [idx if token not in unfixed_tokens else 0
-                        for token, idx in token2id.items()]
-        unfixed_idxmap = [idx if token in unfixed_tokens else 0
-                          for token, idx in token2id.items()]
-        fixed_embedding = nn.Embedding.from_pretrained(
-            torch.Tensor(initial_vectors[fixed_idxmap]), freeze=True)
-        unfixed_embedding = nn.Embedding.from_pretrained(
-            torch.Tensor(initial_vectors[unfixed_idxmap]), freeze=False)
-        embed = EmbeddingUnit(fixed_embedding, unfixed_embedding)
-    else:
-        embed = nn.Embedding.from_pretrained(
-            torch.Tensor(initial_vectors), freeze=True)
-    return embed, unk_freq
+
+def finetune_embedding(config, word_freq, token2id, initial_vectors, all_df):
+    tokens = all_df.tokens.values
+    w2v = Word2Vec(size=300, min_count=1, workers=1)
+    w2v.build_vocab_from_freq(word_freq)
+    idxmap = np.array([token2id[w] for w in w2v.wv.index2word])
+    w2v.wv.vectors[:] = initial_vectors[idxmap]
+    w2v.trainables.syn1neg[:] = initial_vectors[idxmap]
+    w2v.train(tokens, total_examples=len(tokens), epochs=5)
+    finetuned_vectors = w2v.wv.vectors[idxmap.argsort()]
+    return finetuned_vectors
 
 
 class Encoder(nn.Module):
@@ -66,6 +82,6 @@ class Encoder(nn.Module):
     def forward(self, X, mask):
         h = self.embedding(X)
         h = self.dropout(h)
-        h = self.encoder(h)
+        h = self.encoder(h, mask)
         h = self.aggregator(h, mask)
         return h
