811bb1119d27c60d6ef4043e9bb5d242accb4f43
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2019-02-18 02:31:20 +0900

    Move model_selection modules

--- qiqc/model_selection/__init__.py
+++
@@ -1,4 +0,0 @@
-from qiqc.model_selection.results import classification_metrics  # NOQA
-from qiqc.model_selection.results import ClassificationResult  # NOQA
-from qiqc.model_selection.gridsearch import train_gridsearch  # NOQA
-from qiqc.model_selection.optuna import train_optuna  # NOQA
---
+++ qiqc/training/__init__.py
@@ -0,0 +1,3 @@
+from qiqc.training.model_selection.results import classification_metrics  # NOQA
+from qiqc.training.model_selection.results import ClassificationResult  # NOQA
+from qiqc.training.model_selection.gridsearch import train_gridsearch  # NOQA
