860c319bcc7bbf1db262e9515578d457e992a970
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2019-02-18 02:30:44 +0900

    Update config builder

--- qiqc/config.py
+++ qiqc/config.py
@@ -1,20 +1,68 @@
-import operator
-from functools import reduce
+import argparse
+from abc import ABCMeta, abstractmethod
 from pathlib import Path
 
-import yaml
 
+class ExperimentConfigBuilderBase(metaclass=ABCMeta):
 
-def build_config(args):
-    confpath = Path(args.modeldir) / 'config.yml'
-    config = yaml.load(open(confpath))
-    config = {**config, **vars(args)}
-    return config
+    default_config = None
 
+    def add_args(self, parser):
+        parser.add_argument('--modeldir', '-m', type=Path, required=True)
+        parser.add_argument('--device', '-g', type=int)
+        parser.add_argument('--test', action='store_true')
+        parser.add_argument('--logging', action='store_true')
+        parser.add_argument('--outdir', '-o', type=str, default='test')
+        parser.add_argument('--n-rows', type=int)
 
-def get_by_path(config, items):
-    return reduce(operator.getitem, items, config)
+        parser.add_argument('--seed', type=int, default=1029)
+        parser.add_argument('--optuna-trials', type=int)
+        parser.add_argument('--gridsearch', action='store_true')
+        parser.add_argument('--holdout', action='store_true')
+        parser.add_argument('--cv', type=int, default=5)
+        parser.add_argument('--cv-part', type=int)
+        parser.add_argument('--processes', type=int, default=2)
 
+        parser.add_argument('--lr', type=float, default=1e-3)
+        parser.add_argument('--batchsize', type=int, default=512)
+        parser.add_argument('--batchsize-valid', type=int, default=1024)
+        parser.add_argument('--scale-batchsize', type=int, nargs='+',
+                            default=[])
+        parser.add_argument('--epochs', type=int, default=5)
+        parser.add_argument('--validate-from', type=int)
+        parser.add_argument('--pos-weight', type=float, default=1.)
+        parser.add_argument('--maxlen', type=float, default=72)
+        parser.add_argument('--vocab-mincount', type=float, default=5)
+        parser.add_argument('--ensembler-n-snapshots', type=int, default=1)
 
-def set_by_path(config, items, value):
-    get_by_path(config, items[:-1])[items[-1]] = value
+    @abstractmethod
+    def modules(self):
+        raise NotImplementedError()
+
+    def build_outdir(self, modeldir):
+        return Path('results') / '/'.join(modeldir.parts[1:])
+
+    def build(self, args=None):
+        assert self.default_config is not None
+        parser = argparse.ArgumentParser()
+        self.add_args(parser)
+        parser.set_defaults(**self.default_config)
+
+        for module in self.modules:
+            module.add_args(parser)
+        config, extra_config = parser.parse_known_args(args)
+
+        for module in self.modules:
+            if hasattr(module, 'add_extra_args'):
+                module.add_extra_args(parser, config)
+        config = parser.parse_args(args)
+
+        config.outdir = self.build_outdir(config.modeldir)
+        if config.test:
+            config.n_rows = 500
+            config.batchsize = 64
+            config.epochs = 3
+            config.cv_part = 2
+            config.ensembler_test_size = 1.
+
+        return config
