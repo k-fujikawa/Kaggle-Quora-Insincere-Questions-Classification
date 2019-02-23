6c634e5819b440e0eca7da8ce31d45015a1f87c0
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2019-02-23 16:39:11 +0900

    Remove gridsearch

--- qiqc/training/__init__.py
+++ qiqc/training/__init__.py
@@ -1,3 +1,2 @@
 from qiqc.training.model_selection.results import classification_metrics  # NOQA
 from qiqc.training.model_selection.results import ClassificationResult  # NOQA
-from qiqc.training.model_selection.gridsearch import train_gridsearch  # NOQA
